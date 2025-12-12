a3f92cdbfb9c081c3fc462ceab7332a6e83b7933
kadraman <4321872+kadraman@users.noreply.github.com>
2025-12-12 13:22:05 +0000

    SSC updates

--- ssc/start.sh
+++ ssc/start.sh
@@ -1,2 +1,20 @@
 #/bin/bash
-docker compose up -d
+
+# Choose a secure password and store it safely
+export HTTP_CERT_PWD="F)rt1fy!"
+
+# Create a self-signed server cert + private key in a keystore
+rm -rf ./certs
+mkdir certs
+keytool -genkeypair \
+  -keyalg RSA -keysize 2048 \
+  -keystore ./certs/sscKeystore.jks \
+  -alias ssc-server \
+  -storepass "$HTTP_CERT_PWD" \
+  -keypass "$HTTP_CERT_PWD" \
+  -dname "CN=ssc.local, OU=IT, O=YourOrg, L=City, ST=State, C=US"
+
+echo -n "F)rt1fy!" > ./certs/sscKeystorePassword.txt
+chmod 0644 ./certs/sscKeystorePassword.txt
+
+docker compose --env-file ../.env up -d
